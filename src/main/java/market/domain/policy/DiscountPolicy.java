a82c1bfaf6d54cc9c11572aec03face42e0b5b61
hadasfire <fireh@bgu.ac.il>
2025-04-27 21:34:27 +0300

    added changes

--- src/main/java/market/domain/policy/DiscountPolicy.java
+++
@@ -1,7 +0,0 @@
-package market.domain.policy;
-import market.domain.user.StoreBag;
-
-
-public interface DiscountPolicy {
-    void apply(StoreBag bag);
-}
--- src/main/java/market/domain/policy/PurchasePolicy.java
+++
@@ -1,6 +0,0 @@
-package market.domain.policy;
-import market.domain.store.StoreBag;
-
-public interface PurchasePolicy {
-    void validate(String userId, StoreBag bag) throws RuntimeException;
-}
